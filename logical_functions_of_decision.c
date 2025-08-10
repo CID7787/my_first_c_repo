f800ed32e825cb9b544e96980b09176ea99d1365
CID <mamadjonovfarrukh7787@gmail.com>
2025-08-10 20:50:08 +0500

    added ternary_function into logical_functions_of_decision.c

--- logical_functions_of_decision.c
+++ logical_functions_of_decision.c
@@ -2,3 +2,8 @@ long unsigned int else0(unsigned int cond, int x){
     long unsigned int array[2] = {x, 0};
     return array[!cond];
 }
+
+long unsigned int ternary_function(unsigned char condition, long unsigned int true_value, long unsigned int false_value){
+    long unsigned int array[2] = {true_value, false_value};
+    return array[!condition];
+}
