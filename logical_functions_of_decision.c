b6e8e0198e7fc1e1e158c4c3fc6e51dff4608f06
CID <mamadjonovfarrukh7787@gmail.com>
2025-08-10 20:52:49 +0500

    renamed ternary_function in logical_functions_of_decision to ternary

--- logical_functions_of_decision.c
+++ logical_functions_of_decision.c
@@ -3,7 +3,7 @@ long unsigned int else0(unsigned int cond, int x){
     return array[!cond];
 }
 
-long unsigned int ternary_function(unsigned char condition, long unsigned int true_value, long unsigned int false_value){
+long unsigned int ternary(unsigned char condition, long unsigned int true_value, long unsigned int false_value){
     long unsigned int array[2] = {true_value, false_value};
     return array[!condition];
 }
