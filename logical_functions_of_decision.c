20d59e7ee02e7eff55bada33c4a5209ae7f1fd51
CID <mamadjonovfarrukh7787@gmail.com>
2025-08-21 21:08:08 +0500

    wrote the new else0 function through operators

--- logical_functions_of_decision.c
+++ logical_functions_of_decision.c
@@ -1,7 +1,13 @@
-long unsigned int else0(unsigned int cond, int x){
+long unsigned int else0_array(unsigned int cond, int x){
     long unsigned int array[2] = {x, 0};
     return array[!cond];
 }
+
+int else0(int cond, int x){
+    return x & (~0 + !cond);
+}
+
+
 
 long unsigned int ternary(unsigned char condition, long unsigned int true_value, long unsigned int false_value){
     long unsigned int array[2] = {true_value, false_value};
@@ -11,11 +17,9 @@ long unsigned int ternary(unsigned char condition, long unsigned int true_value,
 int trueXfalse0(unsigned int cond, int x){
     int any = (!cond) << (sizeof(int) + 1);
     return x >> any;// if(cond){return x;} else{ return 0;}
-  }
+}
   
-  void ifnot(void* a, void* b){
-    int* c = (int*)a; 
-    int* d = (int*)b;
-    //    if(!(*c)){ *c = *d; }    
-    *c = ternary(*c, *d, *c);
-  }
+void ifnot(void* a, void* b){
+  int* array[2] = {(int*)a, (int*)b};
+  *(array[0]) = array[!(*(array[0]))];
+}
