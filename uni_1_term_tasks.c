42c711b2adc2745ac8dcc0d9e67cc83c93c47219
CID <mamadjonovfarrukh7787@gmail.com>
2026-02-12 02:08:56 +0500

    solved uni OJ problem of solving Hanoi tower for n plates problem

--- uni_1_term_tasks.c
+++ uni_1_term_tasks.c
@@ -14,66 +14,85 @@ https://yandex.com/search/?text=type+of+constants+in+c&lr=202
 https://en.cppreference.com/w/c/language/statements.html
 https://en.cppreference.com/w/c/io/fscanf.html
 https://yandex.com/search/?text=c+fgets&lr=202
-
-*/
-
-/* observe the principle of this recursion
-#define lluint long long unsigned int
-lluint comb(int n, int k){
-    if(k > n){ return 0; }
-    if(k < 1){ return 1; }
-    return (lluint)comb(n - 1, k - 1) + comb(n - 1, k);
-}
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #define uint unsigned int
-/*2599
+#define uch unsigned char
+
+/*1245
 Description
 
-有一楼梯共M级，若每次只能跨上一级或二级，要走上第M级，共有多少种走法？
+“Hanoi Tower”问题的背景和搬移规则大家是否都很熟悉了？为了突出重点，我把问题描述放在下面的HINT部分，不了解的同学可以参考。
 
------------------------------------------------------------------------------
+首先我们Hanoi塔上的盘子按从上到下编号，假设Hanoi塔上有n个盘子，那么最小的那个盘子就是1号盘子，然后是2号、3号……最大的盘子是n号。
 
-Invalid Word（禁用单词）错误：在解决这个题目时，某些关键词是不允许被使用的。如果提交的程序中包含了下列的关键词之一，就会产生这个错误。
+Hanoi塔的3根针我们也进行编号，最左边的是1号，中间的是2号，最右边的是3号。
 
-被禁用的关键字：for, while, do, break, continue, goto。
+如果我们想把n=2个盘子从1号针搬到2号针，那么3号针作为暂存使用。整个搬移过程是这样的：
+
+1号盘子：从1号针搬到3号针
+2号盘子：从1号针搬到2号针
+1号盘子：从3号针搬到2号针
+
+你的任务是编个程序把上面的搬移过程输出来，程序需要输入盘子的个数n，并且这n个盘子一开始在哪根针，要搬到哪根针都是从输入得到的。
 
 Input
 
-输入数据每行包含一个整数M（1<=M<=30）,表示楼梯的级数，至EOF结束。
+输入为多行，至EOF结束。
+
+每行输入四个整数，第一个整数为盘子数n（1<=n<=10），后面的三个整数是三根针的编号，它们排列的顺序是有不同含义的：第二个整数是n个盘子一开始的位置，第四个整数是盘子最终要放置的位置，第三个整数是搬移过程中用来暂存盘子的。
+如：
+输入“1 2 3 1”表示只有一个盘子，从第2根针搬到第1跟针上。
 
 Output
 
-对于每个测试实例，请输出不同走法的数量
+每一行输入都对应一个搬移过程，首先输出一个“case i”，表示对应的第i个输入。然后再它后面输出搬移的步骤。如：
 
-Sample Input
-1
-2
-3
-Sample Output
-1
-2
-3
+输入“1 2 3 1”表示只有一个盘子，从第2根针搬到第1跟针上。那么它的搬移步骤只有一步：把1号盘子从第2跟针搬到第1跟针，即输出：
+
+plate 1 : from 2 to 1
 */
 
-uint steps(uint n){
-    return n < 4 ? n : steps(n - 1) + steps(n - 2);
+
+
+uint circle3(uint num){ return num % 3 ? num % 3 : 3; }  
+
+uint move_n_pl(uint pl){
+    uint a = ~pl;
+    a &= ++pl;
+    for(pl = 0; a; ++pl) 
+        a >>= 1;
+    return pl;
 }
 
 int main(){
-    uint n;
-    while(scanf("%u", &n) ^ EOF){
-        printf("%u\n", steps(n));
+    uint cases = 1, till, i, i1, i2, n, pl, stick[3];
+    uch step1, step2, *stick_pl[4];
+    while(scanf("%u%u%u%u", &pl, stick, stick + 1, stick + 2) ^ EOF){
+        printf("case %u :\n", cases);
+        for(i = till = 0; i < pl; ++i){ till |= (uch)1 << i; }
+        step1 = 1 + ((pl & 1 && circle3(stick[0] + 2) == stick[2]) | (!(pl & 1) && circle3(stick[0] + 1) == stick[2]));
+        ++pl;
+        for(i = 1; i < 4; ++i){ stick_pl[i] = calloc(pl, sizeof(uch)); }
+        for(i = 1; i < pl; ++i){ stick_pl[stick[0]][i] = i; }
+        i = 0;
+        while(i < till){
+            step2 = step1;
+            n = move_n_pl(i++);
+            for(i1 = 1; !stick_pl[i1][n]; ++i1){ }
+            pl = circle3(i1 + step1);
+            for(i2 = 1; stick_pl[pl][i2] < stick_pl[i1][n]; ++i2){ 
+                if(stick_pl[pl][i2]){ step2 += step1; break; } 
+            }
+            pl = circle3(i1 + step2);
+            stick_pl[pl][n] = stick_pl[i1][n];
+            stick_pl[i1][n] = 0;
+            printf("   plate %u : from %u to %u\n", n, i1, pl);
+        }
+        for(i = 1; i < 4; ++i){ free(stick_pl[i]); }
+        cases++;
     }
+    return 0;
 }
-
-/*
-how were hyperbolic functions derived
-https://www.ms.uky.edu/~droyster/courses/fall11/MA341/Classnotes/Lecture%2027%20Handouts.pdf
-https://www.cuemath.com/calculus/hyperbolic-functions/
-https://math.stackexchange.com/questions/718863/geometric-meanings-of-hyperbolic-cosine-and-sine
-https://math.stackexchange.com/questions/455480/how-were-hyperbolic-functions-derived-discovered
-https://www.cuemath.com/geometry/latus-rectum/
-*/
